f4f2fa1f3edbcd3590247b30a425e7709ae755ae
suisse <romain.bedert@gmail.com>
2022-09-14 14:57:10 +0200

    essai sur class Mesures

--- resultats.py
+++ resultats.py
@@ -55,34 +55,58 @@ On fera le XML dans un autre fichier .py
 
 """
 
-
 import os
 import csv
 from configparser import ConfigParser
 import pandas as pd
 
 
+class Mesures(object):
+    """
+    Class pour la lecture des informations des fichiers "Mesures"
+    """
 
+    def __init__(self):
+        self.mesures = ConfigParser()
+        self.path_mesures_file = None
 
+    def read(self):
+        self.mesures.read(self.path_mesures_file)
 
+    def set_path(self, path_mesures_file):
+        self.path_mesures_file = path_mesures_file
+        file_exist = self.mesures.read(self.path_mesures_file)
+        if not file_exist:
+            self.path_mesures_file = None
+            raise ValueError('Aucun fichier de mesure trouvé')
+
+    def get_sections(self):
+        if self.path_mesures_file:
+            self.read()
+            sections = self.mesures.sections()
+            for section in sections:
+                print(section)
+                print(self.mesures.items(section))
+                print(type(self.mesures.items(section)))
 
 
 if __name__ == '__main__':
-
-
     # test lecture fichier mesures
+    # path = os.path.abspath(
+    #     "C:\\Users\\CRMC\\PycharmProjects\\Datamet\\Exemple résultat\\ISO 643_INT_277171_2022-06-07_10-59-04\\277171_Mesures.txt")
     path = os.path.abspath(
-        "C:\\Users\\CRMC\\PycharmProjects\\Datamet\\Exemple résultat\\ISO 643_INT_277171_2022-06-07_10-59-04\\277171_Mesures.txt")
+        r"E:\Romain\Documents\Romain bidouille\Informatique\Taf\Datamet\Exemple résultat\ISO 643_INT_277171_2022-06-07_10-59-04\277171_Mesures.txt")
+    # Mesures = ConfigParser()
+    # Mesures.read(path)
+    # print(Mesures.get('General', 'Module'))
 
-    Mesures = ConfigParser()
-    Mesures.read(path)
-    print(Mesures.get('General', 'Module'))
+    test = Mesures(path)
+    # test.read(path)
+    # test.mesures.get('General', 'Module')
 
-
-    # Lecture fichier de résultats
-    with open("C:\\Users\\CRMC\\PycharmProjects\\Datamet\\Exemple résultat\\ASTM E112_IMA_283558_2022-06-07_13-44-05\\283558_Resultats.txt", newline='') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=";")
-        for row in reader:
-            for key in row:
-                print(key, ' -> ', row[key])
-
+    # # Lecture fichier de résultats
+    # with open("C:\\Users\\CRMC\\PycharmProjects\\Datamet\\Exemple résultat\\ASTM E112_IMA_283558_2022-06-07_13-44-05\\283558_Resultats.txt", newline='') as csvfile:
+    #     reader = csv.DictReader(csvfile, delimiter=";")
+    #     for row in reader:
+    #         for key in row:
+    #             print(key, ' -> ', row[key])
